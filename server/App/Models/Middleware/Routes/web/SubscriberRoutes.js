e7c417a60dbb174ebcc31b141f74ad1322bf57b6
officialsanju2004 <godsanju21@gmail.com>
2026-03-05 17:58:41 +0530

    Update SubscriberRoutes.js

--- server/App/Models/Middleware/Routes/web/SubscriberRoutes.js
+++ server/App/Models/Middleware/Routes/web/SubscriberRoutes.js
@@ -1,5 +1,5 @@
 let express=require('express');
-const { subscriberDataInsert, subscriberDataView, subscriberDelete } = require('../controllers/web/subscriberDataFolder');
+const { subscriberDataInsert, subscriberDataView, subscriberDelete } = require('../controllers/web/SubscriberDataFolder');
 let subscriberRoutes=express.Router(); 
 subscriberRoutes.post("/subscriber-insert",subscriberDataInsert)
 subscriberRoutes.get("/subscriber-view", subscriberDataView);
