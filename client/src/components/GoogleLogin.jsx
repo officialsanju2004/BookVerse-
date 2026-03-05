4d68ef4c3ff7eb6a3e590b0cf153211967522336
officialsanju2004 <godsanju21@gmail.com>
2026-03-05 18:20:34 +0530

    Update GoogleLogin.jsx

--- client/src/components/GoogleLogin.jsx
+++ client/src/components/GoogleLogin.jsx
@@ -5,7 +5,7 @@ const GoogleLoginButton = () => {
   const handleSuccess = async (credentialResponse) => {
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/google",
+        "https://bookverse-6s2i.onrender.com/api/auth/google",
         { token: credentialResponse.credential }
       );
 
